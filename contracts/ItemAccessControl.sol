709f26ec181deebea72dadbef20ae81ac61a2824
seanlarkin99 <60394327+seanlarkin99@users.noreply.github.com>
2022-06-22 15:59:17 +0200

    Add files via upload

--- contracts/ItemAccessControl.sol
+++ contracts/ItemAccessControl.sol
@@ -4,12 +4,28 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//**************************** NEEDS TO BE MADE UPGRADABLE **************************************
+//IDEALLY ONLY UPGRADABLE BY CEO FOR NOW
+/*Would aslo be cool for users to be able to vote to remove a CEO/Exec
+Whether that's game players (or token holders?) or only executives or whatever, good to add an element of democracy to it
+That's why Polkadot is the place to be :))
+*/
 contract ItemAccessControl is Ownable{
-
+    //event ContractUpgrade(address newContract);
+    //that's potentially very interesting but that's down the line :))
 
     address public ceoAddress;
-    
-    address[5] public MinecraftExecutiveAddresses;
+    //let these addresses correspoond to official roles, eg index [0] = MinecraftCEO, [1] = MinecraftCTO etc.
+    //could also be an option to declare an array of fixed size, good for numerous reasons:
+    //1. It limits potential iterations through the array saving on gas
+    //2. It reduces the likelihood of a hostile attack if there is only ever a certain number of addresses with exec privileges
+    //3. It's also obviously requires less memory
+    //4. Every address could have a specific purpose and accountability/responsibility
+    //Arguments against that are as follows:
+    //1. if big orgs want to allow lots of people exec privilege to reduce the burden on one then they can't be limited by array length
+    address[5] public MinecraftExecutiveAddresses;//set it initally as CEO address?
+    //address public cfoAddress;
+    //address public cooAddress;
 
     // @dev Keeps track whether the contract is paused. When that is true, most actions are blocked
     bool public paused = false;
@@ -49,7 +65,8 @@ contract ItemAccessControl is Ownable{
     ) external onlyMinecraftExecutive{
         require(_newMinecraftExecutive != address(0));
         require(!isAlreadyMinecraftExecutive(_newMinecraftExecutive), "This account is already a minecraft executive");
-        
+        // add the following if address array is dynamic: MinecraftExecutiveAddresses.push(_newMinecraftExecutive);
+        //use the below while the array is fixed size (tested with fixed size = 5):
         uint i = 1;// i = 1 to start after CEO address which is at i=0
         bool executiveSet = false;
         while((i< MinecraftExecutiveAddresses.length) && (!executiveSet)){
@@ -81,6 +98,19 @@ contract ItemAccessControl is Ownable{
             i++;
         }
 
+        /*   use the below if the exec addresses array is dynamic: 
+        while((i<MinecraftExecutiveAddresses.length) && (!successfullyRemovedAddress)){
+            if(MinecraftExecutiveAddresses[i] == _minecraftExecToBeRemoved){
+                delete MinecraftExecutiveAddresses[i];
+                successfullyRemovedAddress = true;
+            }
+            if((successfullyRemovedAddress) && (i+1<MinecraftExecutiveAddresses.length)){
+                MinecraftExecutiveAddresses[i] = MinecraftExecutiveAddresses[i+1];
+            }
+            i++;
+        }
+        MinecraftExecutiveAddresses.pop();
+        */
     }
 
     function setMinecraftCEO(
@@ -101,6 +131,17 @@ contract ItemAccessControl is Ownable{
         MinecraftExecutiveAddresses[0] = _newMinecraftCEO;
     }
 
+    /*/// @dev Access modifier for CFO-only functionality
+    modifier onlyCFO() {
+        require(msg.sender == cfoAddress);
+        _;
+    }
+
+    /// @dev Access modifier for COO-only functionality
+    modifier onlyCOO() {
+        require(msg.sender == cooAddress);
+        _;
+    }*/
 
     modifier onlyCLevel() {
         require(
@@ -119,6 +160,23 @@ contract ItemAccessControl is Ownable{
         ceoAddress = _newCEO;
     }
 
+    /*/// @dev Assigns a new address to act as the CFO. Only available to the current CEO.
+    /// @param _newCFO The address of the new CFO
+    function setCFO(address _newCFO) external onlyCEO {
+        require(_newCFO != address(0));
+
+        cfoAddress = _newCFO;
+    }
+
+    /// @dev Assigns a new address to act as the COO. Only available to the current CEO.
+    /// @param _newCOO The address of the new COO
+    function setCOO(address _newCOO) external onlyCEO {
+        require(_newCOO != address(0));
+
+        cooAddress = _newCOO;
+    }*/
+
+    /*** Pausable functionality adapted from OpenZeppelin ***/
 
     /// @dev Modifier to allow actions only when the contract IS NOT paused
     modifier whenNotPaused() {
